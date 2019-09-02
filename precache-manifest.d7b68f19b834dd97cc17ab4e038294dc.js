cbd43f713e88570c8dd0612cde2c3b7a077aa63a
Edson Augusto Silva Catugy <edymbh@gmail.com>
2019-09-02 20:48:19 -0300

    Updates

---
+++ precache-manifest.d7b68f19b834dd97cc17ab4e038294dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dccee033dd2c6c37b7f1daaf0082ad4",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "b5fafaa7216475ebe0a9",
+    "url": "/hangman/static/css/main.fd37794e.chunk.css"
+  },
+  {
+    "revision": "abb27780ffc5b0b39093",
+    "url": "/hangman/static/js/2.7c52c28a.chunk.js"
+  },
+  {
+    "revision": "b5fafaa7216475ebe0a9",
+    "url": "/hangman/static/js/main.095671d4.chunk.js"
+  },
+  {
+    "revision": "2991143a71108c760777",
+    "url": "/hangman/static/js/runtime~main.0aa055f4.js"
+  }
+]);
